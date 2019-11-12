0a8dd98d1f8c29a6c0c05372c07eae69cfe8585f
Rapeapach Suwasri <kagaminelenkyun@gmail.com>
2019-11-12 18:44:41 +0700

    Add Component implementation to component

--- src/Loopback4OpenApiFix.component.ts
+++ src/Loopback4OpenApiFix.component.ts
@@ -1,10 +1,10 @@
-import { bind, ContextTags, inject, CoreBindings } from '@loopback/core';
+import { bind, ContextTags, inject, CoreBindings, Component } from '@loopback/core';
 import { Loopback4OpenApiFixBindings } from './keys';
 import { RestApplication } from '@loopback/rest';
 import { Lb4OpenApiSpec } from './lib/Lb4OpenApiSpec';
 
 @bind({tags: {[ContextTags.KEY]: Loopback4OpenApiFixBindings.COMPONENT}})
-export class Loopback4OpenApiFixComponent {
+export class Loopback4OpenApiFixComponent implements Component {
     constructor(
         @inject(CoreBindings.APPLICATION_INSTANCE)
         private application: RestApplication,        
